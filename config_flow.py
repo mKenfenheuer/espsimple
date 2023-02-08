b34fa7edaab641e24897c01da416fe9c2d09dc1d
Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>
2023-02-08 13:26:58 +0000

    Fix

--- config_flow.py
+++ config_flow.py
@@ -25,7 +25,6 @@ _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("host"): str,
-        vol.Optional("encryption_key"): str,
     }
 )
 
@@ -94,10 +93,29 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle user-confirmation of discovered node."""
+        if user_input is not None:
+            return self.async_show_form(
+                step_id="encryption_key",
+                data_schema=vol.Schema({vol.Required("encryption_key"): str}),
+                description_placeholders={"name": self._name},
+            )
+        errors = {}
+        try:
+            info = await validate_input(self.hass, user_input)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title=info["title"], data=user_input)
+
         return self.async_show_form(
-            data_schema=STEP_USER_DATA_SCHEMA,
             step_id="discovery_confirm",
-            description_placeholders={"host": self._host},
+            description_placeholders={"name": self._name},
+            errors=errors,
         )
 
     async def async_step_zeroconf(
